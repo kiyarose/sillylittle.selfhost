88abe38baa193aed47341ca629739fc50723e1e4
Damian Swan AAHS <damianmswan@icloud.com>
2024-05-26 22:57:14 -0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'SLF - Self Hosting Docs',
+    description: 'Docs for SLF Self Hosting',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
